4f20c039874be8739499f6465ec3fbd96ea0ad83
Rahul Kumar <rk27112001k@gmail.com>
2021-07-09 01:11:15 +0530

    Login component is working fine

---
+++ src/components/login/Login.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import "./Login.css";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+const Login=(props)=>{
+return (
+            <div className="login-wrap">
+                <div className="login-html">
+                    <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/><label for="tab-1" className="tab">Sign In</label>
+                    <input id="tab-2" type="radio" name="tab" className="sign-up"/><label for="tab-2" className="tab">Sign Up</label>
+                    <div className="login-form">
+                        <div className="sign-in-htm">
+                            <div className="group">
+                                <label for="email" className="label">Registered Email *</label>
+                                <input id="email" name="email" type="text" className="input" onChange={props.LoginEmailChange}/>
+                            </div>
+                            <div className="group">
+                                <label for="pass" className="label">Password *</label>
+                                <input id="pass" name="password" type="password" className="input" data-type="password" onChange={props.LoginPasswordChange}/>
+                            </div>
+                            <div className="group">
+                                <input id="check" type="checkbox" className="check" checked/>
+                                <label for="check"><span className="icon"></span> Keep me Signed in</label>
+                            </div>
+                            <div className="group">
+                                <input type="submit" className="button" value="Sign In" onClick={props.SignIn} disabled={props.Button_disable}/>
+                            </div>
+
+                            <div className={
+                                (props.Signin_error=='')? '':
+                                (props.Signin_error=='successful')?'success':
+                                (props.Signin_error=='connecting to server.......')?'info':'error'
+                            }>
+                                {props.Signin_error}
+                            </div>
+
+                            <div className="hr"></div>
+                            <div className="foot-lnk">
+                                <a href="#forgot">Forgot Password?</a>
+                            </div>
+                        </div>
+                        <div className="sign-up-htm">
+                            <table>
+                                <th>
+                                    <td>
+                                    <div className="group">
+                                        <label for="first_name" className="label">First Name *</label>
+                                         <input id="fist_name" name="first_name" type="text" className="input" onChange={props.FirstNameChange} />
+                                    </div>
+                                    </td>
+                                    <td>
+                                    <div className="group">
+                                    <label for="last_name" className="label">Last Name</label>
+                                    <input id="last_name" name="last_name" type="text" className="input" onChange={props.LastNameChange}/>
+                                     </div>
+                                    </td>
+                                </th>
+                            </table>
+
+                            <table>
+                                <th>
+                                    <td>
+                                    <div className="group">
+                                        <label for="pass" className="label">Password *</label>
+                                        <input id="pass" name="password" type="password" className="input" data-type="password" onChange={props.PasswordChange}/>
+                                    </div>
+                                    </td>
+                                    <td>
+                                    <div className="group">
+                                        <label for="pass" className="label">Confirm Password *</label>
+                                        <input id="pass" name="repassword" type="password" className="input" data-type="password" onChange={props.RepasswordChange}/>
+                                    </div>
+                                    </td>
+                                </th>
+                            </table>
+                            <div className="group">
+                                <label for="pass" className="label">Email Address *</label>
+                                <input id="pass" name="email" type="text" className="input" onChange={props.EmailChange}/>
+                            </div>
+                            <div className="group">
+                                <label className="label">Gender *</label>
+                            </div>
+                            <div>
+                                <label for="Male" className="label">Male</label>
+                                <input type="radio" id="Male" value="M" name="gender" onChange={props.GenderChange}/>
+                                <label for="Female" className="label">Female</label>
+                                <input type="radio" id="Female" value="F" name="gender" onChange={props.GenderChange}/>
+                                <label for="Other" className="label">Other</label>
+                                <input type="radio" id="Other" value="O" name="gender" onChange={props.GenderChange}/>
+                            </div> <br />
+                            <div className="group">
+                                <label for="contact_number" className="label">Contact Number *</label>
+                                <input id="contact_number" name="contact_number" type="text" className="input" onChange={props.ContactNumberChange}/>
+                            </div>
+                            <div className="group">
+                                    <input type="submit" className="button" value="Sign Up" onClick={props.SignUp} disabled={props.Button_disable}/>
+                            </div>
+                            <div className={
+                                (props.Signup_error=='')? '':
+                                (props.Signup_error=='Successfully registered!')?'success':
+                                (props.Signup_error=='connecting to server.......')?'info':'error'
+                            }>{props.Signup_error}</div>
+                            <div className="hr"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+export default Login;
---
+++ src/components/user/User.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link, Switch,Redirect } from 'react-router-dom';
+import Login from "../login/Login";
+import Cookies from 'universal-cookie';
+const cookies=new Cookies();
+const signin_url="http://localhost:9000/login";
+const signup_url="http://localhost:9000/signup";
+
+class User extends Component {
+    constructor(){
+        super()
+        this.state={
+            first_name:'',
+            last_name:'',
+            password:'',
+            login_password:'',
+            repassword:'',
+            email:'',
+            login_email:'',
+            gender:'',
+            contact_number:'',
+            button_disable:false,
+            signin_error:'',
+            signup_error:''
+        }
+    }
+    onFirstNameChange=(event)=>{
+        this.setState({first_name:event.target.value});
+    }
+    onLastNameChange=(event)=>{
+        this.setState({last_name:event.target.value});
+    }
+    onPasswordChange=(event)=>{
+        this.setState({password:event.target.value});
+    }
+    onLoginPasswordChange=(event)=>{
+        this.setState({login_password:event.target.value});
+    }
+    onRepasswordChange=(event)=>{
+        this.setState({repassword:event.target.value});
+    }
+    onEmailChange=(event)=>{
+        this.setState({email:event.target.value});
+    }
+    onLoginEmailChange=(event)=>{
+        this.setState({login_email:event.target.value});
+    }
+    onGenderChange=(event)=>{
+        this.setState({gender:event.target.value});
+    }
+    onContactNumberChange=(event)=>{
+        this.setState({contact_number:event.target.value});
+    }
+    onSignIn=(event)=>{
+        const {login_email,login_password}=this.state;
+        if(!login_email || !login_password ){
+            this.setState({signin_error:"* is the required field"});
+        }
+        else {
+            const requestMetadata = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({'email':login_email,
+                                        'password':login_password})
+            };
+            this.setState({button_disable:true});
+            this.setState({signin_error:"connecting to server......."});
+            fetch(signin_url, requestMetadata)
+            .then(res => res.json())
+            .then(res =>{
+                    if(res.error){
+                        this.setState({signin_error:JSON.stringify(res.error)});
+                    }
+                    else {
+                        this.setState({signin_error:"successful"});
+                        cookies.set('isAuth',true,{ path: '/' });
+                        cookies.set('user_token', res.user, { path: '/' });
+                        console.log(cookies.get('user_token'));
+                    }
+                    this.setState({button_disable:false});
+            });
+        }
+    }
+    onSignUp=(event)=>{
+            const {first_name,last_name,password,repassword,email,gender,contact_number}=this.state;
+            if(!first_name || !password || !email || !gender || !contact_number ){
+                this.setState({signup_error:"* is the required field"});
+            }
+            else if(password!=repassword){
+                this.setState({signup_error:"Password and confirm password do not match"});
+            }
+            else {
+                const requestMetadata = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({"first_name":first_name,
+                                            "last_name":last_name,
+                                            "password":password,
+                                            "email":email,
+                                            "Gender":gender,
+                                            "contact_number":contact_number})
+                };
+                this.setState({button_disable:true});
+                this.setState({signup_error:"connecting to server.......",});
+                fetch(signup_url,requestMetadata)
+                .then(res=>res.json())
+                .then(res=>{
+                    if(res.error){
+                        this.setState({signup_error:res.error});
+                    }
+                    else {
+                        this.setState({signup_error:"Successfully registered!"})
+                    }
+                    this.setState({button_disable:false});
+                })
+            }
+            
+    }
+    render(){
+        return (
+            <div>
+                <Login 
+                            FirstNameChange={this.onFirstNameChange}
+                            LastNameChange={this.onLastNameChange}
+                            PasswordChange={this.onPasswordChange}
+                            LoginPasswordChange={this.onLoginPasswordChange}
+                            RepasswordChange={this.onRepasswordChange}
+                            EmailChange={this.onEmailChange}
+                            LoginEmailChange={this.onLoginEmailChange}
+                            GenderChange={this.onGenderChange}
+                            ContactNumberChange={this.onContactNumberChange}
+                            SignIn={this.onSignIn}
+                            SignUp={this.onSignUp}
+                            Signin_error={this.state.signin_error}
+                            Signup_error={this.state.signup_error}
+                            Button_disable={this.state.button_disable}/>
+                <div>{JSON.stringify(this.state)}</div>
+            </div>
+                    
+        )
+    }
+}
+export default User;
